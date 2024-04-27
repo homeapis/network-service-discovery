50e02f1f4aeb3bb75366e124fcf7e91a0c214d1c
theoctt <130503991+theoctt@users.noreply.github.com>
2024-04-27 19:24:23 +0200

    Create setup.sh

---
+++ nsd/utilities/setup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# (c) 2024 HomeAPIs.com. All rights reserved.
+
+# The latest version of this install script is accessible via https://setup.homeapis.com/
+# DO NOT USE IF NOT A MEMBER OF HOMEAPIS.COM, INC.
+# USE OUTSIDE OF THE HOMEAPIS NETWORK OF COMPUTERS IS PROHIBITED
+# AND MAY CAUSE UNEXPECTED BUGS AND ERRORS WHEN RUN OUTSIDE OF HAC
+
+printf("Do you still want to run Network Service Discovery (NSD) on this computer?")
